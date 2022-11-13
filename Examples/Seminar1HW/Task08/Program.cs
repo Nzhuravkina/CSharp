1c92d39a70f1cca43bc8cd05f6adc4156ae00454
ashchurok <nastya.zhuravkina10@gmail.com>
2022-11-13 23:02:52 +0300

    Added tast08 to Seminar1 Homework

---
+++ Examples/Seminar1HW/Task08/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Ввeдите число: ");
+string str1 = Console.ReadLine();
+int number1 = int.Parse(str1);
+
+int i = 1;
+
+if (number1 <= 1)
+{
+    Console.WriteLine("Четных чисул от 1 до " + number1 + " нет");
+}
+else
+{
+    Console.WriteLine("Четные число от 1 до " + number1 + ":");
+
+    while (i <= number1)
+    {
+        if (i % 2 == 0) Console.WriteLine(i);
+        i++;
+    }
+}

38b534c16f1cac21e1ec569210e3cd8412e74b0d
ashchurok <nastya.zhuravkina10@gmail.com>
2022-11-13 16:19:59 +0300

    Added Task3 to Seminar1

---
+++ Examples/Seminar1/Task_003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Ввeдите число:");
+string str1 = Console.ReadLine();
+int number1 = int.Parse(str1);
+
+int negativeNumber = number1 * -1;
+
+Console.WriteLine("Целые числа в промежутке между " + (-number1) + " и " + number1 + ": ");
+
+while(negativeNumber <= number1)
+{
+    Console.Write(negativeNumber +" ");
+    negativeNumber++;
+}
+

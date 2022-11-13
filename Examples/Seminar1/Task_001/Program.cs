d93b381782e7ef4bd042f52d353fb4019b39f350
ashchurok <nastya.zhuravkina10@gmail.com>
2022-11-13 15:40:08 +0300

    Added example task on Seminar 1

---
+++ Examples/Seminar1/Task_001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу: Программа получает число с консоли,
+// возводит его в квадрат и выводит на экран
+
+// Вывод сообщения в консоль
+Console.WriteLine("Введите число, которое хотите возвести в квадрат: ");
+// Получаем текст с консоли
+string str = Console.ReadLine();
+// int.Parse - переводит строку(string) в число (int)
+int number = int.Parse(str);
+
+int square = number * number;
+
+Console.Write("Квадрат вашего числа = ");
+Console.WriteLine(square);
+
+// Вместо строк 13 и 14 можно записать Console.WriteLine("Квадрат вашего числа = " + sqare)

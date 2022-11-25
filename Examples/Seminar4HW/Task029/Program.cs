2d2fcef2d3986fea088d9b9e1421ed0f364331b2
ashchurok <nastya.zhuravkina10@gmail.com>
2022-11-25 16:55:21 +0300

    Added Task 29 to Seminar 4 HW

---
+++ Examples/Seminar4HW/Task029/Program.cs
@@ -0,0 +1,17 @@
+﻿// Hапишите программу, которая генерирует массив из 8 элементов - случайных чисел (от 0 до 20) и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 3 -> [1, 2, 5, 7, 19, 6, 1, 3]
+
+
+void FillArray(int[] array)
+{
+    int arrayLength = 8;
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = new Random().Next(0, 21);
+        Console.WriteLine($"array[{i}] = {array[i]}");
+    }
+}
+
+int[] array2 = new int[8];
+
+FillArray(array2);

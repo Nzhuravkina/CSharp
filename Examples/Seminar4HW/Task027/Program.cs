b1180923e2176729bd663e33f7aa8be0f79ef605
ashchurok <nastya.zhuravkina10@gmail.com>
2022-11-25 16:47:44 +0300

    Added Task 27

---
+++ Examples/Seminar4HW/Task027/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(string number)
+{
+    
+    int m = int.Parse(number);
+    int k = -1;
+    int result = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        k = m % 10;
+        result = result + k;
+        m = m / 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+string A = Console.ReadLine();
+
+Console.WriteLine($"Сумма цифр числа {A} = {Sum(A)}");

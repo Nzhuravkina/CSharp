b23554c0d84053f1f1e804426c2d30fe7654186d
ashchurok <nastya.zhuravkina10@gmail.com>
2022-11-27 12:04:48 +0300

    Added New Examples

---
+++ Examples/Example013_Sorting/Program.cs
@@ -0,0 +1,36 @@
+﻿int [] arr = {1, 2, 3, 4, 5, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.WriteLine();
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+Console.WriteLine();
+SelectionSort(arr);
+PrintArray(arr);
+
+// ДЗ: решить эта задачу от большого к маленькому

e690c41fa67beb8d260092265bb5719d7eac74b6
ashchurok <nastya.zhuravkina10@gmail.com>
2022-11-27 19:40:57 +0300

    Added Seminar5

---
+++ Examples/Seminar5/Task001/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: Задайте массив из 10 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите суммы отрицательных и положительных элементов массива.
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+int SumOfNegative(int[] array)
+{
+    int sumOfNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0) sumOfNegative = sumOfNegative + array[i];
+    }
+    return sumOfNegative;
+
+}
+
+int SumOfPositive(int[] array)
+{
+    int sumOfPositive = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) sumOfPositive = sumOfPositive + array[i];
+    }
+    return sumOfPositive;
+
+}
+
+int[] array = new int[10];
+FillArray(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+Console.WriteLine($"Сумма негативных элементов = {SumOfNegative(array)}");
+Console.WriteLine($"Сумма позитивных элементов = {SumOfPositive(array)}");
---
+++ Examples/Seminar5/Task002/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Задайте массив из 10 элементов и положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void ChangeElements(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+ChangeElements(array);
+
+for (int j = 0; j < array.Length; j++)
+{
+    Console.Write($"{array[j]} ");
+}

d90e41fd4017bd52a38876ca4c08baa398082fe9
ashchurok <nastya.zhuravkina10@gmail.com>
2022-12-05 19:13:55 +0300

    Added seminar 7 tasks

---
+++ Examples/Seminar7/Task001/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = m + n;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write(array[m, n]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк массива");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов массива");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+FillArray(array);
+
+PrintArray(array);
---
+++ Examples/Seminar7/Task002/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 2: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+// Например, изначально массив выглядел вот так:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть вот так:
+
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Square(double[,] array)
+{
+    for (int i = 2; i < array.GetLength(0); i = (i + 2))
+    {
+        for (int j = 2; j < array.GetLength(1); j = (j + 2))
+        {
+            array[i,j] = Math.Pow((array[i,j]), 2);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк массива");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов массива");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m,n];
+
+FillArray(array);
+
+Console.WriteLine("Массив из рандомных чисел");
+
+PrintArray(array);
+
+Square(array);
+
+Console.WriteLine("Числа с четными индексами умножили на 2");
+
+PrintArray(array);

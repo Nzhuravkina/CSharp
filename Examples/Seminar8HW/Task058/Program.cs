2b1faf90ce9f24b803d2bf45bd80823c3808f9dd
ashchurok <nastya.zhuravkina10@gmail.com>
2022-12-12 22:42:34 +0300

    Added Task 058

---
+++ Examples/Seminar8HW/Task058/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 58: Задайте два двумерных массива (от 0 до 10). Напишите программу, которая будет находить произведение двух массивов (поэлементное).
+// Например, даны 2 массива:
+// 2 4
+// 3 2
+
+// 3 4
+// 3 3
+// Результирующая матрица будет:
+// 6 16
+// 9 6
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] MatrixProduct(int[,] array1, int[,] array2)
+{
+    int[,] resultArray = new int[array1.GetLength(0), array2.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            resultArray[i, j] = array1[i, j] * array2[i, j];
+        }
+    }
+    return resultArray;
+}
+
+int[,] array1 = new int[2, 2];
+int[,] array2 = new int[2, 2];
+
+FillArray(array1);
+FillArray(array2);
+
+PrintArray(array1);
+Console.WriteLine();
+
+PrintArray(array2);
+Console.WriteLine();
+
+int[,] resultArray = MatrixProduct(array1, array2);
+
+PrintArray(resultArray);

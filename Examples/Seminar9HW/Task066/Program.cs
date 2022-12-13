ceaeb465cca73006838a182baef08629ac7c70ed
ashchurok <nastya.zhuravkina10@gmail.com>
2022-12-13 22:36:51 +0300

    Added Task 066

---
+++ Examples/Seminar9HW/Task066/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumOfNaturalElements(int n, int m)
+{
+    if (n == m)
+        return n;
+    return (n + SumOfNaturalElements(n + 1, m));
+}
+
+Console.WriteLine("Введите число A");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма элементов в промежутке между {n} и {m} = {SumOfNaturalElements(n, m)}");

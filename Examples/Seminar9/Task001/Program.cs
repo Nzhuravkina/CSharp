3e26411c8e4b0f5e4f9d36b8cdf285a6b3294c2f
ashchurok <nastya.zhuravkina10@gmail.com>
2022-12-13 22:21:20 +0300

    Added Seminar 9

---
+++ Examples/Seminar9/Task001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+string NaturalNumbers(int n)
+{
+    if (n == 0)
+        return "0";
+    if (n == 1)
+        return "1";
+    return ($"{n} {NaturalNumbers(n-1)}");
+}
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(NaturalNumbers(n));
+
---
+++ Examples/Seminar9/Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+int Pow(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    if (b == 1)
+        return a;
+    return (a * Pow(a, b - 1));
+}
+
+Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{a} в степени {b} = {Pow(a, b)}");
